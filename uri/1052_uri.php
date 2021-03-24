8b88a1c841a1039bbae4621c1a84f048fb876759
antonio henrique <antoniohenriquedd15@hotmail.com>
2021-03-24 10:31:12 -0300

    teste branchs

---
+++ uri/1052_uri.php
@@ -0,0 +1,30 @@
+<?php
+    $entrada = (int) fgets(STDIN);
+    if ($entrada == 1){
+        echo "January\n";
+    }elseif ($entrada == 2){
+        echo "February\n";
+    }elseif ($entrada == 3){
+        echo "March\n";
+    }elseif ($entrada == 4){
+        echo "April\n";
+    }elseif ($entrada == 5){
+        echo "May\n";
+    }elseif ($entrada == 6){
+        echo "June\n";
+    }elseif ($entrada == 7){
+        echo "July\n";
+    }elseif ($entrada == 8){
+        echo "August\n";
+    }elseif ($entrada == 9){
+        echo "September\n";
+    }elseif ($entrada == 10){
+        echo "October\n";
+    }elseif ($entrada == 11){
+        echo "November\n";
+    }elseif ($entrada == 12){
+        echo "December\n";
+    }
+
+
+?>
